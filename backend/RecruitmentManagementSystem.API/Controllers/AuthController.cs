94e26f3c0ec970f6d2c122f5471e992f866b47f6
joker45op <abhiguj13579@gmail.com>
2025-09-26 18:09:33 +0530

    Initial commit: backend project setup with AuthController and basic API structure

---
+++ backend/RecruitmentManagementSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecruitmentManagementSystem.API.DTOS;
+using RecruitmentManagementSystem.API.Models;
+
+namespace RecruitmentManagementSystem.API.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+            user.UserName = request.UserName;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (request.UserName != user.UserName)
+            {
+                return BadRequest("User Not found");
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = "yeayyyyyyyyyyyyyyyyyyyyyyy";
+            return Ok("Login Sccussfull "+token);
+        }
+    }
+}
